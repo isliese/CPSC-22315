fa16f4dfd3963ae8101b4a195712bf39c4e33f77
Chanran kim <126753671+isliese@users.noreply.github.com>
2024-12-04 15:31:19 -0800

    Add files via upload

---
+++ lab10/lab10-Chanran Kim/bots.py
@@ -0,0 +1,9 @@
+def bot_clerk(item_list):
+    cart_list = []
+    thread_lock = []
+    
+    # Separate the items that have been passed to the clerk into 3 robot fetcher lists
+    if int(item_list)
+
+def bot_fetcher(item_list, cart_list):
+
